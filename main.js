ff9faba424dc9b2c9cdaefc17118ae400e4edd5c
adeel <adeelmanzoor13@gmail.com>
2022-07-19 15:52:12 +0500

    five role active

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ const text = document.querySelector(".select_text");
 const content = document.querySelectorAll(" .main-role-manage-content");
 const roleOwnDiv = document.querySelectorAll(".main-role-manage-own");
 
-// attendance tab
+// tabs
+// attendance mange
 function openAttendance(evt, roleName, color, br) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("main-role-manage-content");
@@ -63,37 +64,37 @@ function openFeeManagement(evt, roleName, color, br) {
   evt.currentTarget.classList.add("activefm");
 }
 //Event Management
-// function openLessonPlaner(evt, roleName, color, br) {
-//   var i, lptabcontent, lptablinks;
-//   lptabcontent = document.getElementsByClassName("LPC");
-//   for (i = 0; i < 4; i++) {
-//     lptabcontent[i].style.display = "none";
-//   }
-//   lptablinks = document.getElementsByClassName("Lesson-Planner");
-//   for (i = 0; i < lptablinks.length; i++) {
-//     lptablinks[i].className = lptablinks[i].className.replace("activel", "");
-//     lptablinks[i].style.borderColor = br;
-//   }
-//   document.querySelector("#lpContainer").style.backgroundColor = color;
-//   document.getElementById(roleName).style.display = "flex";
-//   evt.currentTarget.classList.add("activel");
-// }
+function openEventManagement(evt, roleName, color, br) {
+  var i, emtabcontent, emtablinks;
+  emtabcontent = document.getElementsByClassName("EMC");
+  for (i = 0; i < 4; i++) {
+    emtabcontent[i].style.display = "none";
+  }
+  emtablinks = document.getElementsByClassName("Event-Management");
+  for (i = 0; i < emtablinks.length; i++) {
+    emtablinks[i].className = emtablinks[i].className.replace("activeem", "");
+    emtablinks[i].style.borderColor = br;
+  }
+  document.querySelector("#emContainer").style.backgroundColor = color;
+  document.getElementById(roleName).style.display = "flex";
+  evt.currentTarget.classList.add("activeem");
+}
 // Exam Planner
-// function openLessonPlaner(evt, roleName, color, br) {
-//   var i, lptabcontent, lptablinks;
-//   lptabcontent = document.getElementsByClassName("LPC");
-//   for (i = 0; i < 4; i++) {
-//     lptabcontent[i].style.display = "none";
-//   }
-//   lptablinks = document.getElementsByClassName("Lesson-Planner");
-//   for (i = 0; i < lptablinks.length; i++) {
-//     lptablinks[i].className = lptablinks[i].className.replace("activel", "");
-//     lptablinks[i].style.borderColor = br;
-//   }
-//   document.querySelector("#lpContainer").style.backgroundColor = color;
-//   document.getElementById(roleName).style.display = "flex";
-//   evt.currentTarget.classList.add("activel");
-// }
+function openExamPlanner(evt, roleName, color, br) {
+  var i, eptabcontent, eptablinks;
+  eptabcontent = document.getElementsByClassName("EPC");
+  for (i = 0; i < 4; i++) {
+    eptabcontent[i].style.display = "none";
+  }
+  eptablinks = document.getElementsByClassName("Exam-Planner");
+  for (i = 0; i < eptablinks.length; i++) {
+    eptablinks[i].className = eptablinks[i].className.replace("activeep", "");
+    eptablinks[i].style.borderColor = br;
+  }
+  document.querySelector("#epContainer").style.backgroundColor = color;
+  document.getElementById(roleName).style.display = "flex";
+  evt.currentTarget.classList.add("activeep");
+}
 
 // nav select
 navSelect.onclick = function () {
